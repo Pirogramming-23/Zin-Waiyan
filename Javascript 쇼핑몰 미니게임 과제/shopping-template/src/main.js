a4217c5b9c40eaa11ec0dee0dbdd25f4b49c9f66
ZinYan <zinwaiyan2226@gmail.com>
2025-07-02 18:07:18 +0900

    JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -5,13 +5,28 @@ function loadItems() {
   .then(json => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items){
+    const container = document.querySelector(".items");
+    const html = items.map(item => createHTMLString(item));
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item){
+    return `
+        <li class="item">
+            <img src="img/yellow_p.png" alt="" class="item__thumbnail">
+            <span class="item__description">male, large</span>
+        </li>
+    `;
+}
 
 // main
 loadItems()
     .then(items => {
         console.log(items);
-        // displayItems(items);
+        displayItems(items);
         // setEventListeners(items)
     })
     .catch(console.log);
-
