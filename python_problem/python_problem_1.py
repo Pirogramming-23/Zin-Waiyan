f07f8a479fec7afcb5d3f60ddb25849dea98b010
ZinYan <zinwaiyan2226@gmail.com>
2025-07-04 00:10:18 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -8,32 +8,60 @@ num = 0
 # n = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
 
 # 3단계
-while True:
-    try:
-        n = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-        if n not in [1,2,3]:
-            print("1,2,3 중 하나를 입력하세요")
-        else:
+# while True:
+#     try:
+#         n = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+#         if n not in [1,2,3]:
+#             print("1,2,3 중 하나를 입력하세요")
+#         else:
+#             break
+#     except ValueError:
+#         print("정수를 입력하세요")
+
+# # 4단계
+# for i in range(n):
+#     num += 1
+#     print(f"playerA : {num}")
+
+# # 5단계
+# while True:
+#     try:
+#         n = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+#         if n not in [1,2,3]:
+#             print("1,2,3 중 하나를 입력하세요")
+#         else:
+#             break
+#     except ValueError:
+#         print("정수를 입력하세요")
+
+# for i in range(n):
+#     num += 1
+#     print(f"playerB : {num}")
+
+
+# 6단계
+# 0 -> playerA, 1-> playerB
+turn = 0
+while num<31:
+    player = 'playerA' if turn==0 else 'playerB'
+    while True:
+        try:
+            n = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+            if n not in [1,2,3]:
+                print("1,2,3 중 하나를 입력하세요")
+            else:
+                break
+        except ValueError:
+            print("정수를 입력하세요")
+    
+    for i in range(n):
+        num += 1
+        print(f"{player} : {num}")
+        if num>=31: # 내부 for문 중단
             break
-    except ValueError:
-        print("정수를 입력하세요")
+    
+    if num>=31:
+        break # 외부 while문 중단
 
-# 4단계
-for i in range(n):
-    num += 1
-    print(f"playerA : {num}")
-
-# 5단계
-while True:
-    try:
-        n = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-        if n not in [1,2,3]:
-            print("1,2,3 중 하나를 입력하세요")
-        else:
-            break
-    except ValueError:
-        print("정수를 입력하세요")
-
-for i in range(n):
-    num += 1
-    print(f"playerB : {num}")
+    # 텀 바뀌기
+    turn = 1 - turn
