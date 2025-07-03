9f6f120e195714b2f31c0e1ebc6a5ea309d8f3a7
ZinYan <zinwaiyan2226@gmail.com>
2025-07-04 00:26:58 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,30 +1,33 @@
 # 마지막에 31 부른 사람 -> Lose
 # 한번 부르면 1~3개의 수만 부를 수 있음
-
+import random
 
 def brGame(player,num):
-    while True:
-        try:
-            n = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-            if n not in [1,2,3]:
-                print("1,2,3 중 하나를 입력하세요")
-            else:
-                break
-        except ValueError:
-            print("정수를 입력하세요")
-    
+    if player == "computer":
+        n = random.randint(1,3)
+    else:
+        while True:
+            try:
+                n = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+                if n not in [1,2,3]:
+                    print("1,2,3 중 하나를 입력하세요")
+                else:
+                    break
+            except ValueError:
+                print("정수를 입력하세요")
+        
     for i in range(n):
         num += 1
-        print(f"{player} : {num}")
+        print(f"{player} {num}")
         if num>=31: # 내부 for문 중단
             break
     return num
 
 num = 0
-# 0 -> playerA, 1-> playerB
+# 0 -> computer, 1-> player
 turn = 0
 while num < 31:
-    player = 'playerA' if turn==0 else 'playerB'
+    player = 'computer' if turn==0 else 'player'
     num = brGame(player,num)
 
     if num >=31:
@@ -33,5 +36,5 @@ while num < 31:
     turn = 1 - turn
 
 # 승자 출력
-winner = "playerB" if turn == 0 else "playerA"
+winner = "player" if turn == 0 else "computer"
 print(f"{winner} win!")
