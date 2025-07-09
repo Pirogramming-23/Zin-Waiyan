e556b10668c3657f4cde3489ffa510940ec5caec
ZinYan <zinwaiyan2226@gmail.com>
2025-07-09 20:36:05 +0900

    djangogirls - 애플리케이션 확장하기

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.post_list,name='post_list'), #if someone enter the website at 'http://127.0.0.1:8000/' address, will go to views.post_list   
+    path('post/<int:pk>/',views.post_detail,name='post_detail'),
 ]
