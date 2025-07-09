66bf3b9f40db6a7db15e250e94ea589f2ed233dc
ZinYan <zinwaiyan2226@gmail.com>
2025-07-09 17:19:07 +0900

    djangogirls - Django urls

---
+++ djangogirls/blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.post_list,name='post_list'), #if someone enter the website at 'http://127.0.0.1:8000/' address, will go to views.post_list   
+]
