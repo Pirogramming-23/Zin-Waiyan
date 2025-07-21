ec4c47f1dff7c1887ef143ac4982f6f7b7c372e6
ZinYan <zinwaiyan2226@gmail.com>
2025-07-21 20:51:53 +0900

    [feat] urls 추가

---
+++ piro23_Ajax_Pirostagram/posts/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Posts
+    path('',views.post_list,name='post_list'),
+    path('create_post/',views.create_post,name='create_post'),
+    path('delete_post/<int:pk>/',views.delete_post,name='delete_post'),
+
+    # Likes
+    path('like_post/<int:pk>/',views.like_post,name='like_post'),
+
+    # Comments
+    path('add_comment/<int:pk>/',views.add_comment,name='add_comment'),
+    path('delete_comment/<int:pk>/',views.delete_comment,name='delete_comment'),
+    
+    # Stories
+    path('post_story/',views.post_story,name='post_story'),
+    path('delete_story/',views.delete_story,name='delete_story'),
+
+    # Search
+    path('search_post/',views.search_post,name='search_post'),
+
+    # (Optional) 
+    # path('search_user/', views.search_user, name='search_user'),
+    # path('sort_post/', views.sort_post, name='sort_post'),
+    # path('add_reply/<int:comment_id>/', views.add_reply, name='add_reply'),
+]
